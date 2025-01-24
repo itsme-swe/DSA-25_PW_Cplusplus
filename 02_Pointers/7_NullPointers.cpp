471d5bb5ffa4675cb34c92dc8045149af8c2e174
itsme-swe <me.harshmehra07@gmail.com>
2025-01-24 05:52:13 +0530

    update

---
+++ 02_Pointers/7_NullPointers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  int *ptr = nullptr; // Modern way to store null value inside pointer, instead of using Null
+
+  cout << ptr << endl;
+
+  return 0;
+}
