11fd96e631b7ba378314d847a76185b2c0438e43
itsme-swe <me.harshmehra07@gmail.com>
2025-01-26 12:12:21 +0530

    update

--- 04_Array/1D_Arrays/MoveNegNumbers/Solution.cpp
+++ 04_Array/1D_Arrays/MoveNegNumbers/Solution.cpp
@@ -1,3 +1,52 @@
 /*
-💥 Move all negative numbers to beginning
+💥 Move all negative numbers to end and positive numbers to the beginning with contant extra space.
+
 */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void moveNumbers(vector<int> &nums)
+{
+  int n = nums.size();
+
+  int s = 0;
+  int e = n - 1;
+
+  while (s < e)
+  {
+    if (nums[s] > 0)
+    {
+      s++;
+    }
+    else if (nums[e] < 0)
+    {
+      e--;
+    }
+    else if (nums[s] < 0 && nums[e] > 0)
+    {
+      swap(nums[e], nums[s]);
+      s++;
+      e--;
+    }
+  }
+}
+
+int main()
+{
+  vector<int> vec = {1, -2, 3, -4, -5, 6};
+
+  moveNumbers(vec);
+
+  for (auto ele : vec)
+  {
+    cout << ele << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
+
+// op: 1 6 3 -4 -5 -2
