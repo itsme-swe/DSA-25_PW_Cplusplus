4b12d1acaeb8c3dfc200ad95ee1519733aca62c8
itsme-swe <me.harshmehra07@gmail.com>
2025-01-28 21:58:19 +0530

    update

---
+++ 09_OOPsForInterviews/3_thisPointer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Teacher
+{
+
+public:
+  string name;
+  string subject;
+  string dept;
+
+  // 🔸 Using this pointer used to point the paramters name. Left hand side are data memebers name
+  Teacher(string name, string subject, string dept)
+  {
+    this->name = name;
+    this->subject = subject;
+    this->dept = dept;
+  }
+};
+
+int main()
+{
+
+  return 0;
+}
