138be95c61eed35aeeb158244417137db2e0ff8f
itsme-swe <me.harshmehra07@gmail.com>
2025-01-29 16:07:23 +0530

    update

---
+++ 10_BinarySearch/Questions/3_FindLowerBound/Solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  int arr[] = {2, 4, 8, 10, 14, 18, 24};
+
+  int n = sizeof(arr) / sizeof(int);
+
+  int target = 19;
+
+  return 0;
+}
