c859a32506bec7b5f37c39f318edebc7de4ee90e
itsme-swe <me.harshmehra07@gmail.com>
2025-01-31 21:05:13 +0530

    update

---
+++ 11_Strings/String_Built-inFunctions/LengthOfString.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  string msg = "Hello World";
+
+  cout << msg.size() << endl; // 11
+
+  cout << msg.length() << endl; // 11
+
+  return 0;
+}
