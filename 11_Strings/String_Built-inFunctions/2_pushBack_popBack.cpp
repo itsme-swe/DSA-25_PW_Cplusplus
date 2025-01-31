6d62155454f8b542b0e5b59a79c7fa1af27b35ce
itsme-swe <me.harshmehra07@gmail.com>
2025-01-31 21:25:35 +0530

    update

---
+++ 11_Strings/String_Built-inFunctions/2_pushBack_popBack.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  string name = "Harsh";
+
+  name.push_back('u'); // Used to add char in string
+  name.push_back('j');
+  name.push_back('i');
+
+  cout << name << endl; // Harshuji
+
+  name.pop_back(); // used to remove last char from string
+
+  cout << name << endl; // Harshuj
+
+  return 0;
+}
