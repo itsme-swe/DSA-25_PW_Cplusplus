679b270fb58ccc78a5bbc6067cf3155b28338f7e
itsme-swe <me.harshmehra07@gmail.com>
2025-02-01 16:53:39 +0530

    update

--- 11_Strings/QuestionsOnStrings/7_MaxOccuringCharacters/Solution.cpp
+++ 11_Strings/QuestionsOnStrings/7_MaxOccuringCharacters/Solution.cpp
@@ -8,15 +8,18 @@ int main()
 
   string s = "leetcode";
 
+  // Create a vector of size 26 (for lowercase English letters) initialized with 0
   vector<int> arr(26, 0);
 
+  // Iterate through the string to count the frequency of each character
   for (int i = 0; i < s.length(); i++)
   {
-    char ch = s[i];
-    int ascii = (int)ch;
-    arr[ascii - 97]++;
+    char ch = s[i];      // Extract the current character
+    int ascii = (int)ch; // Get the ASCII value of the character
+    arr[ascii - 97]++;   // Map 'a' to 0, 'b' to 1, ..., 'z' to 25 and increment count
   }
 
+  // Find the maximum frequency in the array
   int max = 0;
   for (int i = 0; i < arr.size(); i++)
   {
@@ -24,6 +27,7 @@ int main()
       max = arr[i];
   }
 
+  // Iterate through the array to find and print the character(s) with the highest frequency
   for (int i = 0; i < arr.size(); i++)
   {
     if (arr[i] == max)
