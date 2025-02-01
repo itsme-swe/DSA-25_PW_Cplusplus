5d992cae4e0c3cdcbd90e4302ff878a1b2fe9dd4
itsme-swe <me.harshmehra07@gmail.com>
2025-02-01 16:35:55 +0530

    update

---
+++ 11_Strings/QuestionsOnStrings/7_MaxOccuringCharacters/Solution.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+  string s = "leetcode";
+
+  vector<int> arr(26, 0);
+
+  for (int i = 0; i < s.length(); i++)
+  {
+    char ch = s[i];
+    int ascii = (int)ch;
+    arr[ascii - 97]++;
+  }
+
+  int max = 0;
+  for (int i = 0; i < arr.size(); i++)
+  {
+    if (arr[i] > max)
+      max = arr[i];
+  }
+
+  for (int i = 0; i < arr.size(); i++)
+  {
+    if (arr[i] == max)
+    {
+      int ascii = i + 97;
+      char ch = (char)ascii;
+      cout << ch << ":" << max << endl;
+    }
+  }
+
+  return 0;
+}
