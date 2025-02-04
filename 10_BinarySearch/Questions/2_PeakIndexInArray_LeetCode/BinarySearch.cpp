f351d3fe45881b0a008fefc38b67795e316c4a7a
itsme-swe <me.harshmehra07@gmail.com>
2025-02-04 07:29:23 +0530

    update

--- 10_BinarySearch/Questions/2_PeakIndexInArray_LeetCode/BinarySearch.cpp
+++ 10_BinarySearch/Questions/2_PeakIndexInArray_LeetCode/BinarySearch.cpp
@@ -9,11 +9,40 @@ LeetCode(852) - https://leetcode.com/problems/peak-index-in-a-mountain-array/des
 */
 
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+int peakIndexInMountainArray(vector<int> &nums)
+{
+  int n = nums.size();
+
+  int st = 1;
+  int ed = n - 2;
+
+  while (st <= ed)
+  {
+    int mid = st + (ed - st) / 2;
+
+    if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+      return mid;
+    else if (nums[mid] > nums[mid + 1])
+      ed = mid - 1;
+    else
+      st = mid + 1;
+  }
+
+  return -1;
+}
+
 int main()
 {
+  vector<int> arr = {0, 2, 1, 0};
 
+  int ans = peakIndexInMountainArray(arr);
+
+  cout << "The peak index is: " << ans << endl;
   return 0;
 }
+
+// The peak index is: 1
