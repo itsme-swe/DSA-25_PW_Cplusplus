5b362baf362034ca4da4a35f95a70ea4355ad41c
itsme-swe <me.harshmehra07@gmail.com>
2025-02-04 14:35:42 +0530

    update

--- 10_BinarySearch/Questions/5_SingleElementInSortedArray_Leetcode/Solution.cpp
+++ 10_BinarySearch/Questions/5_SingleElementInSortedArray_Leetcode/Solution.cpp
@@ -25,19 +25,47 @@ int singleNonDuplicate(vector<int> &nums)
 
   while (st <= ed)
   {
-    int mid = st + (ed - st);
+    int mid = st + (ed - st) / 2;
 
     if (mid == 0 && nums[0] != nums[1])
       return nums[mid];
     else if (mid == n - 1 && nums[n - 1] != nums[n - 2])
       return nums[mid];
-      
+
+    if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1])
+      return nums[mid];
+
+    else if (mid % 2 == 0)
+    {
+      if (nums[mid - 1] == nums[mid])
+        ed = mid - 1;
+      else
+        st = mid + 1;
+    }
+    else
+    {
+      if (mid % 2 != 0)
+      {
+        if (nums[mid - 1] == nums[mid])
+          st = mid + 1;
+        else
+          ed = mid - 1;
+      }
+    }
   }
+
+  return -1;
 }
 
 int main()
 {
   vector<int> arr = {3, 3, 7, 7, 10, 11, 11};
 
+  int ele = singleNonDuplicate(arr);
+
+  cout << "The single element is: " << ele << endl;
+
   return 0;
 }
+
+// The single element is: 10
