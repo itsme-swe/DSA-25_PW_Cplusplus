7a66b8e4226a305ccd3ebc184b6c05baa3593c44
itsme-swe <me.harshmehra07@gmail.com>
2025-02-04 08:52:00 +0530

    update

---
+++ 10_BinarySearch/Questions/5_SingleElementInSortedArray_Leetcode/Solution.cpp
@@ -0,0 +1,23 @@
+/*
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+Return the single element that appears only once.
+
+Your solution must run in O(log n) time and O(1) space.
+
+LeetCode(540) - https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int singleNonDuplicate(vector<int> &nums) {}
+
+int main()
+{
+  vector<int> arr = {3, 3, 7, 7, 10, 11, 11};
+
+  return 0;
+}
